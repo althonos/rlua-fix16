a5d68f087160676c165a7baaa7e075d9a0b4229e
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-06-28 06:00:08 +0000

    Fix `luai_nummod` using operators instead of macro functions

--- lua/llimits.h
+++ lua/llimits.h
@@ -285,7 +285,7 @@ typedef unsigned long Instruction;
 */
 #if !defined(luai_nummod)
 #define luai_nummod(L,a,b,m)  \
-  { (m) = l_mathop(fmod)(a,b); if ((m)*(b) < 0) (m) += (b); }
+  { (m) = l_mathop(fmod)(a,b); if ((luai_nummul(L,m,b)) < 0) (m) += (b); }
 #endif
 
 /* exponentiation */
